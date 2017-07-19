b7de30f4c58b1d146ac72e97f395bb9dfbfe627b
DevRoss <820584458@qq.com>
2017-07-19 16:29:49 +0800

    Corrected annotation of filter and input layer

--- cnn/inference.py
+++ cnn/inference.py
@@ -4,11 +4,11 @@ import tensorflow as tf
 '''
 如果是输入层 [a, b, c, d]中a表示batch， b c d 分别表示的的是 输入图片的长、宽和深度
 '''
-input_layer = tf.placeholder(tf.float32, [None, 5, 5, 3], name='image_input')
+input_layer = tf.placeholder(tf.float32, [None, 32, 32, 3], name='image_input')
 
 # filter layer
 '''
-过滤器，shape 表示 像素点为 5x5 的彩色图片(3当前深度，即表示 RGB三原色)，16 为输出深度
+过滤器，shape 表示  5x5 过滤器，(3表示当前深度，即表示 RGB三原色)，16 为输出深度
 '''
 filter_weight = tf.Variable(tf.truncated_normal(shape=[5, 5, 3, 16], stddev=0.1), name='filter_weight')
 # biases

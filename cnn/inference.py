a47f54d71732bdbed3a63c45e5f09c7f7f88fa95
DevRoss <820584458@qq.com>
2017-07-19 15:35:35 +0800

    add 2d convolution

--- cnn/inference.py
+++ cnn/inference.py
@@ -4,3 +4,6 @@ import tensorflow as tf
 filter_weight = tf.Variable(tf.truncated_normal(shape=[5, 5, 3, 16], stddev=0.1), name='filter_weight')
 # biases
 biases = tf.Variable(tf.constant(0.1), name='biases')
+
+# convolution
+conv = tf.nn.conv2d(input, filter_weight, strides=[1, 1, 1, 1], padding='SAME')

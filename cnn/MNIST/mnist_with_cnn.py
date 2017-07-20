2f56eea9513dfb161061721fa2e766df969aad47
DevRoss <820584458@qq.com>
2017-07-20 23:04:28 +0800

    Fix conv1_biases and conv2_biases

--- cnn/MNIST/mnist_with_cnn.py
+++ cnn/MNIST/mnist_with_cnn.py
@@ -26,7 +26,7 @@ def inference(input_tensor, train, regularizer):
     with tf.variable_scope('layer1-conv1'):
         conv1_weight = tf.Variable(tf.truncated_normal([CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_DEPTH], stddev=0.1),
                                    name='conv1_weight')
-        conv1_biases = tf.Variable(tf.constant(CONV1_DEPTH), name='conv1_biases')
+        conv1_biases = tf.Variable(tf.constant(0.0, shape=[CONV1_DEPTH]), name='conv1_biases')
         conv1 = tf.nn.conv2d(input_tensor, conv1_weight, [1, 1, 1, 1], padding='SAME')
         relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
 
@@ -38,6 +38,6 @@ def inference(input_tensor, train, regularizer):
     with tf.variable_scope('layer3-conv2'):
         conv2_weight = tf.Variable(tf.truncated_normal([CONV2_SIZE, CONV1_SIZE, CONV1_DEPTH, CONV2_DEPTH], stddev=0.1),
                                    name='conv2_weight')
-        conv2_biases = tf.Variable(tf.constant(CONV2_DEPTH), name='conv2_biases')
+        conv2_biases = tf.Variable(tf.constant(0.0, shape=[CONV1_DEPTH]), name='conv2_biases')
         conv2 = tf.nn.conv2d(pool1, conv2_weight, [1, 1, 1, 1], padding='SAME')
         relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))

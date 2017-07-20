539d2e5fcbba1a378f5ec0334fe1c57da50cb1a2
DevRoss <820584458@qq.com>
2017-07-20 21:41:02 +0800

    testcase

---
+++ cnn/MNIST/cnn.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+import tensorflow as tf
+
+# convolution and pooling
+
+def conv(input, conv_strides, )
---
+++ cnn/testing.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class cnn(TestCase):
+    def setUp(self):
+        pass
+
+    def test_cnn(self):
+        pass
+
+    def tearDown(self):
+        pass

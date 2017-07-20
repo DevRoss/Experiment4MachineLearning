bd0bd27098c7c358b10b0c9f1a845905a7119250
DevRoss <820584458@qq.com>
2017-07-20 22:47:48 +0800

    first cnn layer (filter1)

--- cnn/MNIST/mnist_with_cnn.py
+++ cnn/MNIST/mnist_with_cnn.py
@@ -19,3 +19,13 @@ CONV2_DEPTH = 64
 
 # Full connected neural network
 FC_SIZE = 512
+
+
+def inference(input_tensor, train, regularizer):
+    # First cnn layer
+    with tf.variable_scope('cnn_1'):
+        conv1_weight = tf.Variable(tf.truncated_normal([CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_DEPTH], stddev=0.1),
+                                   name='cnv1_weight')
+        conv1_biases = tf.Variable(tf.constant(CONV1_DEPTH), name='cnv1_biases')
+        conv1 = tf.nn.conv2d(input_tensor, conv1_weight, [1, 1, 1, 1], padding='SAME')
+        relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))

47d582ce0c06707ecff49c3ac1458eb19dbe95f3
DevRoss <820584458@qq.com>
2017-07-20 22:53:16 +0800

    second layer for pooling

--- cnn/MNIST/mnist_with_cnn.py
+++ cnn/MNIST/mnist_with_cnn.py
@@ -22,10 +22,14 @@ FC_SIZE = 512
 
 
 def inference(input_tensor, train, regularizer):
-    # First cnn layer
-    with tf.variable_scope('cnn_1'):
+    # First cnn layer for convolution
+    with tf.variable_scope('layer1-conv1'):
         conv1_weight = tf.Variable(tf.truncated_normal([CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_DEPTH], stddev=0.1),
                                    name='cnv1_weight')
         conv1_biases = tf.Variable(tf.constant(CONV1_DEPTH), name='cnv1_biases')
         conv1 = tf.nn.conv2d(input_tensor, conv1_weight, [1, 1, 1, 1], padding='SAME')
         relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
+
+    # Second cnn layer for pooling
+    with tf.name_scope('layer2-pool1'):
+        pool1 = tf.nn.max_pool(relu1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')

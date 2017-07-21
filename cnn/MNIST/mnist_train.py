61c1fca009f0bc7f24c78401dd9b5556dfe18a69
DevRoss <820584458@qq.com>
2017-07-21 10:00:51 +0800

    Regularizer

--- cnn/MNIST/mnist_train.py
+++ cnn/MNIST/mnist_train.py
@@ -2,6 +2,8 @@ from .mnist_with_cnn import *
 import tensorflow as tf
 
 BATCH_SIZE = 100
+REGULARIZATION_RATE = 0.0001  # 正则化损失函数的
+
 
 def train(mnist):
     # 图片输入
@@ -13,3 +15,5 @@ def train(mnist):
                             NUM_CHANNELS], name='x-input')
         y_ = tf.placeholder(tf.float32, [BATCH_SIZE, OUT_NODE], name='y-input')
 
+    # 正则化
+    regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)

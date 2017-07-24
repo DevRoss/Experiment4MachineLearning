484ccba77815b65778b1ad1b3d22f8edafe573a7
DevRoss <820584458@qq.com>
2017-07-24 20:57:06 +0800

    reshape and accuracy

--- cnn/MNIST/mnist_eval.py
+++ cnn/MNIST/mnist_eval.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import sys
 import os
+import numpy as np
 
 sys.path.append(os.path.abspath(os.path.curdir))
 
@@ -25,3 +26,18 @@ def evaluate(mnist):
                             NUM_CHANNELS], name='x-input')
 
         y_ = tf.placeholder(tf.float32, [BATCH_SIZE, OUT_NODE], name='y-input')
+        # 改变维度
+        xs = np.reshape(mnist.validation.images,
+                        [mnist.validation.num_examples,
+                         IMAGE_SIZE,
+                         IMAGE_SIZE,
+                         NUM_CHANNELS
+                         ])
+        ys = mnist.validation.labels
+        validate_feed = {x: xs, y_: ys}
+
+        # 向前传播得到结果
+        y = inference(xs)
+
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))

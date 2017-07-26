a8acdac79ebb609211c054279ccd677c3809c1ec
DevRoss <820584458@qq.com>
2017-07-26 16:21:26 +0800

    读取特征向量

--- migrate_learning/get_graph.py
+++ migrate_learning/get_graph.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.python.platform import gfile
 import os
 from data_collection import get_image
 import numpy as np
@@ -36,7 +37,10 @@ BATCH = 100
 
 # 经过Inception v3 处理后特征的存放位置
 def get_bottleneck_path(result, label_name, index, category):
-    return get_image(result, label_name, index, category) + '.txt'
+    image_path = get_image(result, label_name, index, category)
+    basename = os.path.basename(image_path)
+    cache_file = os.path.join(CACHE_DIR, label_name, basename + '.txt')
+    return cache_file
 
 
 # 传入图片，后的特征向量
@@ -45,4 +49,33 @@ def run_bottleneck_on_image(sess, image_data, image_data_tensor, bottleneck_tens
 
     # 经过卷积后变成一个四维数组，将其压缩成一个一维向量
     bottleneck_values = np.squeeze(bottleneck_values)
+    return bottleneck_values
+
+
+# 缓存机制，如果特征向量已经保存，则返回特征向量，否则先存入文件，再返回特征向量。
+def get_or_create_bottleneck(sess, result, label_name, index, category, jpeg_data_tensor, bottleneck_tensor):
+    label_lists = result[label_name]
+    sub_dir_path = os.path.join(CACHE_DIR, label_name)
+    if not os.path.exists(sub_dir_path):
+        os.makedirs(sub_dir_path)
+    bottleneck_path = get_bottleneck_path(result, label_name, index, category)
+
+    # 如果特征向量文件不存在
+    if not os.path.exists(bottleneck_path):
+        image_path = get_image(result, label_name, index, category)
+        # 获取图片内容
+        image_data = gfile.FastGFile(image_path, 'rb').read()
+        # 获取特征向量
+        bottleneck_values = run_bottleneck_on_image(sess, image_data, jpeg_data_tensor, bottleneck_tensor)
+        # 将特征向量存入文件
+        bottleneck_string = ','.join(str(x) for x in bottleneck_values)
+        with open(bottleneck_path, 'w') as bottleneck_file:
+            bottleneck_file.write(bottleneck_string)
+
+    else:
+        # 从文件中读取特征向量
+        with open(bottleneck_path, 'r') as bottleneck_file:
+            bottleneck_string = bottleneck_file.read()
+        bottleneck_values = [float(x) for x in bottleneck_string.split(',')]
+
     return bottleneck_values

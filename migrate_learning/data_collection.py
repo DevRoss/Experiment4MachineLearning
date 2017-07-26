91e55e8b6fa2eef8c650cb74d325f09ed8d6711c
DevRoss <820584458@qq.com>
2017-07-26 13:12:01 +0800

    封装成函数

--- migrate_learning/data_collection.py
+++ migrate_learning/data_collection.py
@@ -7,17 +7,26 @@ import os
 import glob
 from collections import OrderedDict
 
-path = os.path.join(os.path.abspath(os.path.curdir), 'flower_photos')
 
-tree = os.walk(path)
+def get_data_as_dict(folder)
+    '''
+    获取数据
+    :param folder: flower 存放的文件夹
+    :return: 一个花名和图片地址的键值对
+    '''
+    path = os.path.join(os.path.abspath(os.path.curdir), folder)
 
-relpath, dirs, files = next(tree)
+    tree = os.walk(path)
 
-extensions = ['JPG', 'JPEG', 'jpg', 'jpeg']
-flower_collection = OrderedDict()
+    relpath, dirs, files = next(tree)
 
-for dir in dirs:
-    flower_collection[dir] = list()
-    for extension in extensions:
-        flower_path = os.path.join(relpath, dir, '*.' + extension)
-        flower_collection[dir].extend(glob.glob(flower_path))
+    extensions = ['JPG', 'JPEG', 'jpg', 'jpeg']
+    flower_collection = OrderedDict()
+
+    for dir in dirs:
+        flower_collection[dir] = list()
+        for extension in extensions:
+            flower_path = os.path.join(relpath, dir, '*.' + extension)
+            flower_collection[dir].extend(glob.glob(flower_path))
+
+    return flower_collection
